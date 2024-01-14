a084fb6079bf0d226138098f5ab33f8a0746eba1
Loganeek <827467999@qq.com>
2024-01-14 16:33:39 +0800

    set statusbar color to transparent

--- app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sunnyweather.ui.weather
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,8 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sunnyweather.R
@@ -25,6 +28,9 @@ open class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val decorView = window.decorView
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.TRANSPARENT
 
         binding = ActivityWeatherBinding.inflate(layoutInflater)
         setContentView(binding.root)
